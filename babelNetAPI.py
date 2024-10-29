489d12616d8daf0294d704a02efa1e688995f96c
bjones0218 <blake28jones@gmail.com>
2024-10-29 13:20:39 -0500

    adding weights to candidate clues to get single word labels

--- babelNetAPI.py
+++ babelNetAPI.py
@@ -46,30 +46,6 @@ def get_outgoing_edges(synsetId, edgeNum, edgeType):
 
 	if response.status_code == 200:
 		edges = response.json()
-
-		# if edgeNum == 0:
-		# 	hypernyms = [
-		# 		relation for relation in edges['relations']
-		# 		if relation['relationType'] == 'HYPERNYM']
-
-		# 	for result in hypernyms:
-		# 		if result['language'] == 'EN':
-		# 			target = result.get('target') #Target synset
-		# 			language = result.get('language') 
-
-		# 			# retrieving BabelPointer data
-		# 			pointer = result['pointer']
-		# 			relation = pointer.get('name') # hypernym hyponym etc
-		# 			type = pointer.get('shortName') #is-a, part-of, etc.
-		# 			group = pointer.get('relationGroup') #HYPERNYM, HYPONYM, etc.
-
-		# 			print(language \
-		# 			+ "\t" + str(target) \
-		# 			+ "\t" + str(relation) \
-		# 			+ "\t" + str(type) \
-		# 			+ "\t" + str(group))
-			
-
 		# retrieving Edges data
 
 		#CRETAE ARRAY WHERE EVERYTHING IS STORED, TALK WITH BLAKE ABOUT HOW
@@ -81,29 +57,24 @@ def get_outgoing_edges(synsetId, edgeNum, edgeType):
 		for result in edges:
 			if result['language'] == 'EN':
 				target = result.get('target') #Target synset
-				language = result.get('language') 
 
 				# retrieving BabelPointer data
 				pointer = result['pointer']
-				relation = pointer.get('name') # hypernym hyponym etc
 				type = pointer.get('shortName') #is-a, part-of, etc.
 				group = pointer.get('relationGroup') #HYPERNYM, HYPONYM, etc.
-
-				added = False
 
 				if pointer.get('isAutomatic') == False:
 					#Gets all synsets from initial edge
 					if edgeNum == 0:
 						synsetArray.append(result)
-						print("EDGE 0")
+						# print("EDGE 0")
 						edgeOne = get_outgoing_edges(target, 1, type)
 						synsetArray.extend(edgeOne)
-						added = True
 					#Gets only hypernyms for first edge
 					elif edgeNum == 1:
 						if group == 'HYPERNYM':
 							synsetArray.append(result)
-							print("Level 1 hypernym")
+							# print("Level 1 hypernym")
 							edgeTwo = get_outgoing_edges(target, 2, type)
 							synsetArray.extend(edgeTwo)
 							added = True
@@ -111,31 +82,25 @@ def get_outgoing_edges(synsetId, edgeNum, edgeType):
 					elif edgeNum == 2:
 						if type == edgeType:
 							synsetArray.append(result)
-							print("same edge type level 2")
+							# print("same edge type level 2")
 							edgeThree = get_outgoing_edges(target, 3, type)
 							synsetArray.extend(edgeThree)
-							added = True
 					#Stops at third edge but must be same type
 					elif edgeNum == 3:
 						if type == edgeType:
 							synsetArray.append(result)
-							print("same edge type level 3")
-							added = True
-
-					# if added:
-					# 	print(language \
-					# 	+ "\t" + str(target) \
-					# 	+ "\t" + str(relation) \
-					# 	+ "\t" + str(type) \
-					# 	+ "\t" + str(group))
-		
-		print(len(synsetArray))
+							# print("same edge type level 3")
+		# print(len(synsetArray))
 		return synsetArray
 	else:
 		print(f"Error: Unable to fetch data. Status code: {response.status_code}")
 		return None
 	
-def get_single_word_clues(synsetArray):
+def get_single_word_clues(synsetArray, singleWordLabels):
+	W1 = 1.0
+	W2 = 1.1
+	W3 = 1.1
+	W4 = 1.2
 
 	for synset in synsetArray:
 		SYNSET_INFO_URL = 'https://babelnet.io/v9/getSynset'
@@ -153,31 +118,51 @@ def get_single_word_clues(synsetArray):
 			main_sense = senses[0]  # The first sense is usually considered the main sense
 			other_senses = senses[1:]  # The rest are other senses
 
-			# Extract and print main sense
-			main_sense_lemma = main_sense.get('properties', {}).get('fullLemma')
-			main_sense_language = main_sense.get('properties', {}).get('language')
-			main_sense_source = main_sense.get('source')
+			split_main_sense = main_sense.get('properties', {}).get('fullLemma').split("_")
 
-			print(f"Main Sense:\nLemma: {main_sense_lemma}\nLanguage: {main_sense_language}\nSource: {main_sense_source}\n")
+			if len(split_main_sense) == 1 and split_main_sense[0] not in singleWordLabels:
+				singleWordLabels[split_main_sense[0]] = W1
+			else:
+				for word in split_main_sense:
+					if word not in singleWordLabels:
+						singleWordLabels[word] = W2
+			
+			for sense in other_senses:
+				split_other_sense = sense.get('properties', {}).get('fullLemma').split("_")
+				if len(split_other_sense) == 1 and split_other_sense[0] not in singleWordLabels:
+					singleWordLabels[split_other_sense[0]] = W3
+				else:
+					for word in split_other_sense:
+						if word not in singleWordLabels:
+							singleWordLabels[word] = W4
+
+			# Extract and print main sense
+			# main_sense_lemma = main_sense.get('properties', {}).get('fullLemma')
+			# main_sense_language = main_sense.get('properties', {}).get('language')
+			# main_sense_source = main_sense.get('source')
+
+			# print(f"Main Sense:\nLemma: {main_sense_lemma}\nLanguage: {main_sense_language}\nSource: {main_sense_source}\n")
 
 			# Extract and print other senses
-			print("Other Senses:")
-			for sense in other_senses:
-				lemma = sense.get('properties', {}).get('fullLemma')
-				language = sense.get('properties', {}).get('language')
-				source = sense.get('source')
-				print(f"Lemma: {lemma}\tLanguage: {language}\tSource: {source}\n")
-		else:
-			print("No senses found for this synset.")
-
+			# print("Other Senses:")
+			# for sense in other_senses:
+			# 	lemma = sense.get('properties', {}).get('fullLemma')
+			# 	language = sense.get('properties', {}).get('language')
+			# 	source = sense.get('source')
+			# 	print(f"Lemma: {lemma}\tLanguage: {language}\tSource: {source}\n")
+	return singleWordLabels
 
 word = "boat"
 # Get synsets for the word
 synsets = get_synsets(word)
 
+singleWordLabels = {}
+
 if synsets:
 	for synset in synsets:
-		print(synset['id'] + "woohoo")
+		# print(synset['id'] + "woohoo")
 		array = get_outgoing_edges(synset['id'], 0, "")
-		# get_single_word_clues(array)
+		singleWordLabels = get_single_word_clues(array, singleWordLabels)
+
+print(len(singleWordLabels))
 
