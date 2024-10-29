39f8e7ae46554fbcd13ca619565c81d2ce843b34
bjones0218 <blake28jones@gmail.com>
2024-10-28 19:11:09 -0500

    work on creating candidate clues in api

--- babelNetAPI.py
+++ babelNetAPI.py
@@ -88,42 +88,87 @@ def get_outgoing_edges(synsetId, edgeNum, edgeType):
 				type = pointer.get('shortName') #is-a, part-of, etc.
 				group = pointer.get('relationGroup') #HYPERNYM, HYPONYM, etc.
 
-				#Gets all synsets from initial edge
-				if edgeNum == 0:
-					synsetArray.append(result)
-					print("HYPERNYM GROUP WITH EDGE 0")
-					edgeOne = get_outgoing_edges(target, 1, type)
-					synsetArray.extend(edgeOne)
-				#Gets only hypernyms for first edge
-				elif edgeNum == 1:
-					if group == 'HYPERNYM':
-						synsetArray.append(result)
-						print("same edge type")
-						edgeTwo = get_outgoing_edges(target, 2, type)
-						synsetArray.extend(edgeTwo)
-				#Gets same types of words for second edges
-				elif edgeNum == 2:
-					if type == edgeType:
-						synsetArray.append(result)
-						print("same edge type")
-						edgeThree = get_outgoing_edges(target, 3, type)
-						synsetArray.extend(edgeThree)
-				#Stops at third edge but must be same type
-				elif edgeNum == 3:
-					if type == edgeType:
-						synsetArray.append(result)
-						print("same edge type")
+				added = False
 
-				print(language \
-				+ "\t" + str(target) \
-				+ "\t" + str(relation) \
-				+ "\t" + str(type) \
-				+ "\t" + str(group))
+				if pointer.get('isAutomatic') == False:
+					#Gets all synsets from initial edge
+					if edgeNum == 0:
+						synsetArray.append(result)
+						print("EDGE 0")
+						edgeOne = get_outgoing_edges(target, 1, type)
+						synsetArray.extend(edgeOne)
+						added = True
+					#Gets only hypernyms for first edge
+					elif edgeNum == 1:
+						if group == 'HYPERNYM':
+							synsetArray.append(result)
+							print("Level 1 hypernym")
+							edgeTwo = get_outgoing_edges(target, 2, type)
+							synsetArray.extend(edgeTwo)
+							added = True
+					#Gets same types of words for second edges
+					elif edgeNum == 2:
+						if type == edgeType:
+							synsetArray.append(result)
+							print("same edge type level 2")
+							edgeThree = get_outgoing_edges(target, 3, type)
+							synsetArray.extend(edgeThree)
+							added = True
+					#Stops at third edge but must be same type
+					elif edgeNum == 3:
+						if type == edgeType:
+							synsetArray.append(result)
+							print("same edge type level 3")
+							added = True
+
+					# if added:
+					# 	print(language \
+					# 	+ "\t" + str(target) \
+					# 	+ "\t" + str(relation) \
+					# 	+ "\t" + str(type) \
+					# 	+ "\t" + str(group))
 		
+		print(len(synsetArray))
 		return synsetArray
 	else:
 		print(f"Error: Unable to fetch data. Status code: {response.status_code}")
 		return None
+	
+def get_single_word_clues(synsetArray):
+
+	for synset in synsetArray:
+		SYNSET_INFO_URL = 'https://babelnet.io/v9/getSynset'
+		synsetParams = {
+			'id' : synset['target'],
+			'key'  : API_KEY
+		}
+		synsetResponse = requests.get(SYNSET_INFO_URL, params=synsetParams)
+
+		if synsetResponse.status_code == 200:
+			senses = synsetResponse.json()
+			senses = senses.get('senses', [])
+
+		if senses:
+			main_sense = senses[0]  # The first sense is usually considered the main sense
+			other_senses = senses[1:]  # The rest are other senses
+
+			# Extract and print main sense
+			main_sense_lemma = main_sense.get('properties', {}).get('fullLemma')
+			main_sense_language = main_sense.get('properties', {}).get('language')
+			main_sense_source = main_sense.get('source')
+
+			print(f"Main Sense:\nLemma: {main_sense_lemma}\nLanguage: {main_sense_language}\nSource: {main_sense_source}\n")
+
+			# Extract and print other senses
+			print("Other Senses:")
+			for sense in other_senses:
+				lemma = sense.get('properties', {}).get('fullLemma')
+				language = sense.get('properties', {}).get('language')
+				source = sense.get('source')
+				print(f"Lemma: {lemma}\tLanguage: {language}\tSource: {source}\n")
+		else:
+			print("No senses found for this synset.")
+
 
 word = "boat"
 # Get synsets for the word
@@ -132,12 +177,6 @@ synsets = get_synsets(word)
 if synsets:
 	for synset in synsets:
 		print(synset['id'] + "woohoo")
-		get_outgoing_edges(synset['id'], 0, "")
+		array = get_outgoing_edges(synset['id'], 0, "")
+		# get_single_word_clues(array)
 
-# Print the synsets
-if synsets:
-    print(f"Found {len(synsets)} synsets for the word '" + word + "':")
-    for synset in synsets:
-        print(synset)
-else:
-    print("No synsets found.")
