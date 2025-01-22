e4c9b4b2fcbd1549a68df807da2ebf9974fd927b
Marc Eidelhoch <meidelhoch@gmail.com>
2025-01-22 16:37:39 -0600

    played  around with scoring

--- current_scoring.py
+++ current_scoring.py
@@ -1,11 +1,16 @@
 from orig_scoring import original_scoring
 from detect import detect
 
-good_words = ["germany", "car", "change", "glove", "needle", "robin", "belt",
-"board", "africa", "gold"]
-bad_words = ["pipe", "kid", "key", "boom", "satellite", "tap", "nurse", "pyramid", "rock", "bark"]
-clue1 = "oil"
-clue2 = "europe"
+good_words = ["GERMANY", "CAR", "CHANGE", "GLOVE", "NEEDLE", "ROBIN", "BELT",
+"BOARD", "AFRICA", "GOLD"]
+bad_words = ["PIPE", "KID", "KEY", "BOOM", "SATELLITE", "TAP", "NURSE", "PYRAMIDS", "ROCK", "BARK"]
+clue2 = "EUROPE"
 
-print(original_scoring(clue1, good_words, bad_words) + detect(clue1, good_words, bad_words))
-print(original_scoring(clue2, good_words, bad_words) + detect(clue2, good_words, bad_words))
+
+print("The words for your team are:", good_words)
+print("The words for the other team are:", bad_words)
+clue = input("Your clue is: ").upper()
+
+print("Your clue score is:", original_scoring(clue, good_words, bad_words) + detect(clue, good_words, bad_words))
+print("The clue given in the paper for these words is:", clue2)
+print("The score for that word is:", original_scoring(clue2, good_words, bad_words) + detect(clue2, good_words, bad_words))
