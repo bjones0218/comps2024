6ef42533b65c7fea5987a06fe3356f6817d992fd
Marc Eidelhoch <meidelhoch@gmail.com>
2025-02-06 13:31:45 -0600

    added some word stem stuff

---
+++ word_stem_test.py
@@ -0,0 +1,20 @@
+from nltk.stem import WordNetLemmatizer
+
+import spacy  
+
+lemmatizer2 = WordNetLemmatizer()
+
+print(lemmatizer2.lemmatize("SPINAL"))
+print(lemmatizer2.lemmatize("SPINE"))
+
+nlp = spacy.load("en_core_web_sm")
+
+
+text = "spine, spinal, dogs, dog, knives, knife"
+
+# Process the text with spaCy
+doc = nlp(text)
+
+# Extract the lemmatized form of each token
+for token in doc:
+    print(token.lemma_)
